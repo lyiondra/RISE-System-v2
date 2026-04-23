735f0bb6a14e95c261e1d3e3a39a8e4902984132
lyiondra <foreverbeauty2490@gmail.com>
2026-04-23 14:24:51 -0400

    Deployment: Activating workflow steps

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-  base: '/RISE-System-v2/', // Must have slashes on both sides
-  build: {
-    outDir: 'dist',
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+  base: '/RISE-System-v2/', // 👈 This MUST have the slashes!
+})
